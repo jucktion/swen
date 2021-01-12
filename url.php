9ec0060ee8a2ab6f3bd6670321896069129272fc
Jorin Manandhar <nirose@jucktion.com>
2021-01-12 12:50:56 +0545

    change php to 7.4

--- url.php
+++ url.php
@@ -1,9 +1,10 @@
 <?php
 #print_r($_GET);
 if (isset($_GET['url'])){
-    echo 'URL is set';
+    #echo 'URL is set';
     $url = filter_var($_GET['url'], FILTER_SANITIZE_URL);
-    var_dump(filter_var($url, FILTER_VALIDATE_URL));
+    #var_dump(filter_var($url, FILTER_VALIDATE_URL));
+    #echo $url;
     if (filter_var($url, FILTER_VALIDATE_URL)){
         include_once('functions.php');
         $data = getUrl($url);
