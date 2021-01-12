668caf44101e34f494d57b4694428033e9855664
Jorin Manandhar <nirose@jucktion.com>
2021-01-12 12:59:04 +0545

    update composer

--- url.php
+++ url.php
@@ -3,18 +3,14 @@
 if (isset($_GET['url'])){
     #echo 'URL is set';
     $url = filter_var($_GET['url'], FILTER_SANITIZE_URL);
-    if (filter_var($url, FILTER_VALIDATE_URL)){
-        include_once('functions.php');
-        $data = getUrl($url);
-        if($data){
-            echo $data;
-        }
-        else{
-            echo "Couldn\'t get anything";
-        }
+    echo $url;
+    include_once('functions.php');
+    $data = getUrl($url);
+    if($data){
+        echo $data;
     }
     else{
-        echo 'Could not validate URL';
+        echo "Couldn\'t get anything";
     }
 }
 else{
