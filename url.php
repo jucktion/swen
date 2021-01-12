5fdefdd441eed6e63e527b76212b8351ab1ceddc
Jorin Manandhar <nirose@jucktion.com>
2021-01-12 12:26:34 +0545

    update url.php for debug

--- url.php
+++ url.php
@@ -1,16 +1,22 @@
 <?php
 #print_r($_GET);
-if (isset($_GET['url']) && filter_var($_GET['url'], FILTER_VALIDATE_URL)){
-    include_once('functions.php');
-    $data = getUrl($_GET['url']);
-    if($data){
-        echo $data;
+if (isset($_GET['url'])){
+    echo 'URL is set';
+    if (filter_var($_GET['url'], FILTER_VALIDATE_URL)){
+        include_once('functions.php');
+        $data = getUrl($_GET['url']);
+        if($data){
+            echo $data;
+        }
+        else{
+            echo "Couldn\'t get anything";
+        }
     }
     else{
-        echo "Couldn\'t get anything";
+        echo 'Could not validate URL';
     }
 }
 else{
-    echo 'Nothing here...';
+    echo 'Nothing is set...';
 }
 ?>
