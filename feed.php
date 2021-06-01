fd5fe550b4556bb5bc8d2a3bdbf6d2962e4946ee
Jorin Manandhar <nirose@jucktion.com>
2021-06-01 09:08:23 +0545

    change key for feed

--- feed.php
+++ feed.php
@@ -1,6 +1,6 @@
 <?php
 ini_set("max_execution_time", "600");
-if (isset($_GET['k']) && $_GET['k'] == 'run123'){
+if (isset($_GET['k']) && $_GET['k'] == '>f#-jr13{=3^>}u'){
 $stime = microtime(true);
 include_once 'functions.php';
 if (isset($_GET['s'])) {
