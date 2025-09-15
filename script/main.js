7e86ce0a94e29cab89b8842f95f32b18ced78f72
Jorin Manandhar <nirose@jucktion.com>
2025-09-15 12:33:33 +0545

    Revert "minimize js" breaks some logic
    This reverts commit eb36274b1e9a95173af7862c17c00817050aba70.

--- script/main.js
+++ script/main.js
@@ -1,4 +1,6 @@
-function getContent(t,e="GET",s){let i=new XMLHttpRequest;i.open(e,t,!0),i.send(),i.onreadystatechange=function(){4===i.readyState&&200===i.status&&(i.responseText,"function"==typeof s&&s.apply(i))}}let url=new URL(document.URL),rd=url.searchParams.get("r")?"https://"+url.searchParams.get("r"):"https://reddit.com",lang=null===url.searchParams.get("l")?navigator.language:""==url.searchParams.get("l")?navigator.language:url.searchParams.get("l").length>0?url.searchParams.get("l"):"",hash=""!=window.location.hash?window.location.hash.split("#")[1].split(","):"",h1=hash.length>=1?hash[0].toLowerCase():"",h2=hash.length>=2?hash[1].toLowerCase():"";Vue.component("tablet",{template:`
+function getContent(url, methodType = 'GET', callback) { let xhr = new XMLHttpRequest(); xhr.open(methodType, url, true); xhr.send(); xhr.onreadystatechange = function () { if (xhr.readyState === 4) { if (xhr.status === 200) { let resp = xhr.responseText; if (typeof callback === "function") { callback.apply(xhr); } } else { } } else { } } }
+let url = new URL(document.URL); let rd = (url.searchParams.get("r")) ? 'https://' + url.searchParams.get("r") : 'https://reddit.com'; let lg = (url.searchParams.get("l") === null) ? navigator.language : (url.searchParams.get("l") == '') ? navigator.language : (url.searchParams.get("l").length > 0) ? url.searchParams.get("l") : ''; let hash = (window.location.hash != '') ? window.location.hash.split('#')[1].split(',') : ''; let h1 = (hash.length >= 1) ? hash[0].toLowerCase() : ''; let h2 = (hash.length >= 2) ? hash[1].toLowerCase() : ''; Vue.component('tablet', {
+    template: `
     <div>
         <div class="tabs is-centered is-toggle navbar is-toggle-rounded">
             <ul>
@@ -10,11 +12,27 @@ function getContent(t,e="GET",s){let i=new XMLHttpRequest;i.open(e,t,!0),i.send(
         </div>
 
     </div>
-    `,data:()=>({parents:[]}),created(){this.parents=this.$children},mounted(){this.hashActivate()},methods:{selectTab:function(t){this.parents.forEach(e=>{e.isActive=e.title==t.title,e.title==t.title&&e.setChildActive()})},hashActivate:function(){""!=h1?this.parents.forEach(t=>{t.title.toLowerCase()==h1&&(t.isActive=t.selected=!0,""==h2&&t.setChildActive())}):""==h1&&""==h2&&(this.parents[0].isActive=this.parents[0].selected=!0,this.parents[0].setChildActive())}}}),Vue.component("parent",{template:`
+    `, data() { return { parents: [] }; }, created() { this.parents = this.$children; }, mounted() { this.hashActivate(); }, methods: {
+        selectTab: function (selectedtab) { this.parents.forEach(tab => { tab.isActive = (tab.title == selectedtab.title); if (tab.title == selectedtab.title) { tab.setChildActive(); } }); }, hashActivate: function () {
+            if (h1 != '') {
+                this.parents.forEach(tab => {
+                    if (tab.title.toLowerCase() == h1) {
+                        tab.isActive = tab.selected = true; if (h2 == '')
+                            tab.setChildActive();
+                    }
+                });
+            }
+            else if (h1 == '' && h2 == '') { this.parents[0].isActive = this.parents[0].selected = true; this.parents[0].setChildActive(); }
+        }
+    }
+}); Vue.component('parent', {
+    template: `
     <div v-show="isActive" class='tab-details'>
         <slot></slot>
     </div>
-    `,props:{title:{required:!0},selected:{default:!1}},data:()=>({isActive:!1}),mounted(){this.isActive=this.selected},methods:{setChildActive:function(){this.$children[0].selectDef()}}}),Vue.component("tabs",{template:`
+    `, props: { title: { required: true }, selected: { default: false } }, data() { return { isActive: false, } }, mounted() { this.isActive = this.selected; }, methods: { setChildActive: function () { this.$children[0].selectDef(); } }
+}); Vue.component('tabs', {
+    template: `
     <div class="content childcontent">
         <div class="tabs child is-centered is-toggle navbar is-toggle-rounded">
             <ul>
@@ -25,18 +43,54 @@ function getContent(t,e="GET",s){let i=new XMLHttpRequest;i.open(e,t,!0),i.send(
             <slot></slot>
         </div>
     </div>
-    `,data:()=>({tabs:[],isActive:!1}),created(){this.tabs=this.$children},mounted(){h2.length>1&&this.hashActivate(),this.filterTab(this.tabs)},methods:{selectTab:function(t){this.tabs.forEach(e=>{e.isActive=e.current=e.name==t.name,e.name==t.name&&(e.isLoaded||e.setData(),e.current=!0,e.isLoaded&&e.isActive&&e.current&&e.setVoice())})},filterTab:function(t){return this.tags=this.tabs,this.tags.forEach((t,e)=>{t.tag=0==e?"":","+t.name}),this.tags},hashActivate:function(){""!=h2?this.tabs.forEach(t=>{tabname=t.name,t.isActive=t.current=tabname==h2,tabname==h2&&tabname==h2&&(t.isLoaded||t.setData(),t.current=!0,t.isLoaded&&t.isActive&&t.current&&t.setVoice())}):this.tabs[0].isActive=this.tabs[0].selected=!0},selectDef:function(){this.selectTab(this.$children[0])}}}),Vue.component("tab",{template:`
+    `, data() { return { tabs: [], isActive: false }; }, created() { this.tabs = this.$children; }, mounted() {
+        if (h2.length > 1)
+            this.hashActivate(); this.filterTab(this.tabs);
+    }, methods: {
+        selectTab: function (selectedtab) {
+            this.tabs.forEach(tab => {
+                tab.isActive = tab.current = (tab.name == selectedtab.name); if (tab.name == selectedtab.name) {
+                    if (!tab.isLoaded) { tab.setData(); }
+                    tab.current = true;; if (tab.isLoaded && tab.isActive && tab.current) { tab.setVoice(); }
+                }
+            });
+        }, filterTab: function (tabs) { this.tags = this.tabs; this.tags.forEach((tab, index) => { tab.tag = (index == 0) ? '' : ',' + tab.name; }); return this.tags; }, hashActivate: function () {
+            if (h2 != '') {
+                this.tabs.forEach(tab => {
+                    tabname = tab.name; tab.isActive = tab.current = (tabname == h2); if (tabname == h2) {
+                        if (tabname == h2) {
+                            if (!tab.isLoaded) { tab.setData(); }
+                            tab.current = true;; if (tab.isLoaded && tab.isActive && tab.current) { tab.setVoice(); }
+                        }
+                    }
+                });
+            }
+            else { this.tabs[0].isActive = this.tabs[0].selected = true; }
+        }, selectDef: function () { this.selectTab(this.$children[0]); }
+    }
+}); Vue.component('tab', {
+    template: `
     <div :id="this.name.toLowerCase()" v-show="isActive" class='tab-details'>
         <ul>
         <li v-for="link,index in this.linklist">
         <span class="itm">{{index+1}}</span>
         <a v-if="link.rurl" rel="noopener, noreferrer" target="_blank" :href="rd + link.rurl">[{{link.score}}]</a>
         <a v-if="link.com" rel="noopener, noreferrer" target="_blank" :href="link.com">[{{link.score}}]</a>
-        <a  rel="noopener, noreferrer" target="_blank" :href="link.url">{{htmlDecode(link.title)}}</a><span class="del" @click="remove(index)">x</span></li>
+        <a rel="noopener, noreferrer" target="_blank" :href="link.url">{{htmlDecode(link.title)}}</a><span class="del" @click="remove(index)">x</span></li>
         </ul>
         <slot></slot>
     </div>
-    `,props:{name:{required:!0},selected:{default:!1},title:{required:!0},voice:{default:!1}},data(){return{isActive:!1,isLoaded:!1,current:this.voice,linklist:[]}},created(){this.isActive=this.selected},updated(){this.$parent.isActive&&this.isActive&&!this.isLoaded&&(this.current=!0,this.setData(),this.setVoice()),0!=this.linklist.length&&(this.isLoaded=!0),this.current&&this.setVoice()},beforeMount(){this.isLoaded||this.setData()},methods:{setData:function(){if(this.isActive){let t=this;getContent("temp/"+this.name.toLowerCase()+"-parsed.json","GET",function(){t.linklist=JSON.parse(this.responseText)})}},setVoice:function(){this.current&&this.isActive&&(this.$root.voice=this.linklist)},remove:function(t){this.$delete(this.linklist,t)},htmlDecode:function(t){let e=new DOMParser().parseFromString(t,"text/html");return e.documentElement.textContent}}}),Vue.component("voice",{template:`
+    `, props: { name: { required: true }, selected: { default: false }, title: { required: true }, voice: { default: false } }, data() { return { isActive: false, isLoaded: false, current: this.voice, linklist: [] } }, created() { this.isActive = this.selected; }, updated() {
+        if (this.$parent.isActive && this.isActive && !this.isLoaded) { this.current = true; this.setData(); this.setVoice(); }
+        if (this.linklist.length != 0)
+            this.isLoaded = true; if (this.current)
+            this.setVoice();
+    }, beforeMount() {
+        if (!this.isLoaded)
+            this.setData();
+    }, methods: { setData: function () { if (this.isActive) { let now = this; let filename = 'temp/' + this.name.toLowerCase() + '-parsed.json'; getContent(filename, 'GET', function () { now.linklist = JSON.parse(this.responseText); }); } }, setVoice: function () { if (this.current && this.isActive) { this.$root.voice = this.linklist; } }, remove: function (index) { this.$delete(this.linklist, index) }, htmlDecode: function (str) { const doc = new DOMParser().parseFromString(str, "text/html"); return doc.documentElement.textContent; } }
+}); Vue.component('voice', {
+    template: `
 <div v-show="showFab" class="voice-section">
 <div class="fab">
     <div class="settings" @click="settingsShown = !settingsShown"><img src="/img/settings.svg" alt="Voice settings"></div>
@@ -67,4 +121,23 @@ function getContent(t,e="GET",s){let i=new XMLHttpRequest;i.open(e,t,!0),i.send(
     </form>
 </div>
 </div>
-    `,data:()=>({settingsShown:!1,stopShown:!1,showFab:!1,svg:"/img/play.svg",speak:[],saythis:null,pitch:1,rate:1,separator:".... and in other news ....",synth:window.speechSynthesis,voices:[],selectedVoice:0,startItem:1,endItem:25}),mounted(){this.showFab="speechSynthesis"in window,this.showFab&&(this.loadVoices(),this.synth.cancel());window.matchMedia("(max-width: 512px)").matches||window.addEventListener("scroll",this.onScroll)},beforeDestroy(){window.removeEventListener("scroll",this.onScroll)},methods:{loadVoices:function(){new Promise(function(t,e){let s;s=setInterval(()=>{0!==window.speechSynthesis.getVoices().length&&(t(window.speechSynthesis.getVoices()),clearInterval(s))},10)}).then(t=>{this.voices=lg?t.filter(function(t){return -1!=t.lang.indexOf(lang)}):t})},speaker:function(){if(null!=this.saythis||this.synth.speaking||(this.speak=this.$root.voice.map(t=>t.title),this.saythis=this.speak.slice(this.startItem-1,this.endItem).join(this.separator)),this.synth.speaking&&!this.synth.paused){this.synth.pause(),this.playPause(0);return}if(this.synth.paused&&this.synth.speaking){this.synth.resume(),this.playPause(1);return}if(!this.synth.paused&&null!=this.saythis){let t=new SpeechSynthesisUtterance(this.saythis);this.stopShown=!0,now=this,t.onend=function(t){now.stopSpeak()},t.onerror=function(t){console.error("SpeechSynthesisUtterance.onerror")},t.voice=this.voices[this.selectedVoice],t.pitch=this.pitch,t.rate=this.rate,this.synth.speak(t),this.playPause(1)}},stopSpeak:function(){this.synth.cancel(),this.stopShown=!1,this.saythis=null,this.playPause(0)},playPause:function(t){this.svg=0==t?"/img/play.svg":"/img/pause.svg"},onScroll:function(){this.showFab=window.innerHeight+window.scrollY!=document.body.offsetHeight}}});let vm=new Vue({el:"#app",data:()=>({voice:[]})});
+    `, data() { return { settingsShown: false, stopShown: false, showFab: false, svg: '/img/play.svg', speak: [], saythis: null, pitch: 1, rate: 1, separator: '.... and in other news ....', synth: window.speechSynthesis, voices: [], selectedVoice: 0, startItem: 1, endItem: 25 } }, mounted() {
+        this.showFab = ('speechSynthesis' in window); if (this.showFab) { this.loadVoices(); this.synth.cancel(); }
+        let mq = window.matchMedia("(max-width: 512px)").matches; if (!mq)
+            window.addEventListener('scroll', this.onScroll);
+    }, beforeDestroy() { window.removeEventListener('scroll', this.onScroll) }, methods: {
+        loadVoices: function () {
+            function setSpeech() { return new Promise(function (resolve, reject) { let id; id = setInterval(() => { if (window.speechSynthesis.getVoices().length !== 0) { resolve(window.speechSynthesis.getVoices()); clearInterval(id); } }, 10); }) }
+            let s = setSpeech(); s.then((voices) => { this.voices = (lg) ? voices.filter(function (voice) { return voice.lang.indexOf(lg) != -1; }) : voices; });
+        }, speaker: function () {
+            if (this.saythis == null && !this.synth.speaking) { this.speak = this.$root.voice.map(x => x.title); this.saythis = this.speak.slice(this.startItem - 1, this.endItem).join(this.separator); }
+            if (this.synth.speaking && !this.synth.paused) { this.synth.pause(); this.playPause(0); return; }
+            else if (this.synth.paused && this.synth.speaking) { this.synth.resume(); this.playPause(1); return; }
+            else if (!this.synth.paused && this.saythis != null) {
+                let utterThis = new SpeechSynthesisUtterance(this.saythis); this.stopShown = true; now = this; utterThis.onend = function (event) { now.stopSpeak(); }
+                utterThis.onerror = function (event) { console.error('SpeechSynthesisUtterance.onerror'); }
+                utterThis.voice = this.voices[this.selectedVoice]; utterThis.pitch = this.pitch; utterThis.rate = this.rate; this.synth.speak(utterThis); this.playPause(1);
+            }
+        }, stopSpeak: function () { this.synth.cancel(); this.stopShown = false; this.saythis = null; this.playPause(0); }, playPause: function (talking) { this.svg = talking == 0 ? '/img/play.svg' : '/img/pause.svg'; }, onScroll: function () { this.showFab = (window.innerHeight + window.scrollY) != document.body.offsetHeight; }
+    }
+}); let vm = new Vue({ el: '#app', data() { return { voice: [] } } });
